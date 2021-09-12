2800fec00ec8358c7ea96647103921a5d76b2a14
maryada <maryada@pop-os.localdomain>
2021-09-12 12:05:17 +0530

    image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState,useEffect} from 'react';
 
 function App() {
+  const [demo, setDemo]=useState(
+    {
+      
+        "copyright": "Davide Necchi",
+        "date": "2021-09-12",
+        "explanation": "You  ds;kjgireuogore a fool",
+        "hdurl": "https://apod.nasa.gov/apod/image/2109/AuroraIceland_Necchi_1280.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "A Spiral Aurora over Iceland",
+        "url": "https://apod.nasa.gov/apod/image/2109/AuroraIceland_Necchi_960.jpg"
+      
+    }
+  );
+
+  const [loading,setLoading]=useState(false);
+  const getInfo=()=>{
+    setLoading(true);
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then((response) => {
+    // console.log(response.data);
+    setDemo(response.data);
+    setLoading(false);
+    // console.log(response.status);
+    // console.log(response.statusText);
+    // console.log(response.headers);
+    // console.log(response.config);
+  });
+
+ 
+}
+
+useEffect(()=>{
+  getInfo();
+},[])
+
+   if(!loading)
   return (
+  
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={demo.hdurl} style={{width:'500px'}} alt="" />
         <p>
-         Edited by prithika
+        {demo.explanation}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+
+  else
+  return(<div>loading</div>)
 }
 
 export default App;
